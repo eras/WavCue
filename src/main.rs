2c2633e213ff540b4f086cda82d4c65502136139
Erkki Seppälä <flux@inside.org>
2022-05-15 21:06:18 +0300

    Added basic support for BroadcastAudioExtension
    Specified in https://web.archive.org/web/20091229093941/http://tech.ebu.ch/docs/tech/tech3285.pdf

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,33 @@ struct CueEntry {
     sample_start: u32,
 }
 
+// bext: https://web.archive.org/web/20091229093941/http://tech.ebu.ch/docs/tech/tech3285.pdf page 7
+// typedef struct broadcast_audio_extension {
+//   CHAR Description[256]; /* ASCII : «Description of the sound sequence» */
+//   CHAR Originator[32]; /* ASCII : «Name of the originator» */
+//   CHAR OriginatorReference[32]; /* ASCII : «Reference of the originator» */
+//   CHAR OriginationDate[10]; /* ASCII : «yyyy-mm-dd» */
+//   CHAR OriginationTime[8]; /* ASCII : «hh-mm-ss» */
+//   DWORD TimeReferenceLow; /* First sample count since midnight low word */
+//   DWORD TimeReferenceHigh; /* First sample count since midnight, high word */
+//   WORD Version; /* Version of the BWF; unsigned binary number */
+//   BYTE UMID_0 /* Binary byte 0 of SMPTE UMID */
+//   ....
+//   BYTE UMID_63 /* Binary byte 63 of SMPTE UMID */
+//   BYTE Reserved[190] ; /* 190 bytes, reserved for future use, set to “NULL” */
+//   CHAR CodingHistory[]; /* ASCII : « History coding » */
+// } BROADCAST_EXT
+#[derive(Debug)]
+struct BroadcastAudioExtension {
+    description: String,          /* ASCII : «Description of the sound sequence» */
+    originator: String,           /* ASCII : «Name of the originator» */
+    originator_reference: String, /* ASCII : «Reference of the originator» */
+    origination_date: String,     /* ASCII : «yyyy-mm-dd» */
+    origination_time: String,     /* ASCII : «hh-mm-ss» */
+    time_reference: u64,          /* First sample count since midnight */
+    version: u16,                 /* Version of the BWF; unsigned binary number */
+}
+
 #[derive(Debug)]
 struct Header {
     compression_code: u16,
@@ -38,12 +65,14 @@ struct Header {
 struct WaveFileInfo {
     header: Header,
     cues: Vec<CueEntry>,
+    bext: Option<BroadcastAudioExtension>,
 }
 
 fn read_wave(filename: &str) -> Result<WaveFileInfo, std::io::Error> {
     let file = File::open(filename)?;
     let mut reader = BufReader::new(file);
     let mut cues = Vec::new();
+    let mut bext: Option<BroadcastAudioExtension> = None;
     let mut header: Option<Header> = None;
 
     let mut buf_riff: [u8; 4] = [0; 4];
@@ -67,7 +96,48 @@ fn read_wave(filename: &str) -> Result<WaveFileInfo, std::io::Error> {
                 reader.read_exact(&mut buf_chunk32_size)?;
                 let chunk_size = u32::from_le_bytes(buf_chunk32_size);
                 assert!(chunk_size > 0); // TODO: use custom error type
-                if &buf_tag == b"fmt " {
+                if &buf_tag == b"bext" {
+                    let mut buf_bext: [u8; 348] = [0; 348];
+                    assert!(chunk_size as usize >= buf_bext.len()); // TODO: use custom error type
+                    reader.read_exact(&mut buf_bext)?;
+                    reader.seek_relative(chunk_size as i64 - buf_bext.len() as i64)?;
+                    let mut ofs = 0;
+                    let description = String::from_utf8_lossy(array_ref!(buf_bext, ofs, 256))
+                        .trim_end_matches(char::from(0))
+                        .to_string();
+                    ofs += 256;
+                    let originator = String::from_utf8_lossy(array_ref!(buf_bext, ofs, 32))
+                        .trim_end_matches(char::from(0))
+                        .to_string();
+                    ofs += 32;
+                    let originator_reference =
+                        String::from_utf8_lossy(array_ref!(buf_bext, ofs, 32))
+                            .trim_end_matches(char::from(0))
+                            .to_string();
+                    ofs += 32;
+                    let origination_date =
+                        String::from_utf8_lossy(array_ref!(buf_bext, ofs, 10)).to_string();
+                    ofs += 10;
+                    let origination_time =
+                        String::from_utf8_lossy(array_ref!(buf_bext, ofs, 8)).to_string();
+                    ofs += 8;
+                    let time_reference_low = u32::from_le_bytes(*array_ref!(buf_bext, ofs, 4));
+                    ofs += 4;
+                    let time_reference_high = u32::from_le_bytes(*array_ref!(buf_bext, ofs, 4));
+                    ofs += 4;
+                    let version = u16::from_le_bytes(*array_ref!(buf_bext, ofs, 2));
+                    bext = Some(BroadcastAudioExtension {
+                        description,
+                        originator,
+                        originator_reference,
+                        origination_date,
+                        origination_time,
+                        time_reference: time_reference_low as u64
+                            | ((time_reference_high as u64) << 32),
+                        version,
+                    });
+                    eprintln!("{bext:?}");
+                } else if &buf_tag == b"fmt " {
                     let mut buf_fmt: [u8; 16] = [0; 16];
                     assert!(chunk_size >= 16); // TODO: use custom error type
                     assert!(header.is_none()); // TODO: use custom error type
@@ -152,7 +222,7 @@ fn read_wave(filename: &str) -> Result<WaveFileInfo, std::io::Error> {
         None => panic!("No header"), //TODO: use custom error type
     };
 
-    Ok(WaveFileInfo { header, cues })
+    Ok(WaveFileInfo { header, bext, cues })
 }
 
 fn process(filename: &str) -> Result<(), std::io::Error> {
@@ -160,7 +230,20 @@ fn process(filename: &str) -> Result<(), std::io::Error> {
     for cue in wave.cues {
         let sample_start = cue.sample_start;
         let seconds = sample_start as f64 / wave.header.sampling_rate as f64;
-        println!("{:.3},Mark {}", seconds, cue.cue_id);
+        let time_label = match wave.bext {
+            None => String::from(""),
+            Some(BroadcastAudioExtension { time_reference, .. }) => {
+                let time = (time_reference as f64 + sample_start as f64)
+                    / wave.header.sampling_rate as f64;
+                let hour = (time / 3600f64) as u32;
+                let min = (time / 60f64) as u32 % 60u32;
+                let sec = time as u32 % 60u32;
+                let time_fmt = format!(" {}:{:02}:{:02}", hour, min, sec);
+                time_fmt
+            }
+        };
+
+        println!("{:.3},Mark {}{}", seconds, cue.cue_id, time_label);
     }
     Ok(())
 }
